e732099e4a8273ce0ecbb4b1260ac2cfa47c8fcb
yend724 <yend9524@gmail.com>
2025-03-10 22:06:40 +0900

    wip

--- lexer/src/inline.rs
+++
@@ -1,5 +0,0 @@
-fn process_emphasis(input: &str) -> Vec<Token> {
-    let graphemes = input.graphemes(true).collect::<Vec<&str>>();
-    let mut result_tokens = Vec::new();
-    let mut acc_text = String::new();
-}
